831932554acf3a794a7a3269a6b3d68876f3ec89
Yorkie <l900422@vip.qq.com>
2014-01-17 21:18:30 +0800

    tweak quip to debug

--- src/quip.c
+++ src/quip.c
@@ -5,10 +5,21 @@
 //
 
 #include <stdio.h>
+#include <uv.h>
 #include "quip.h"
 
 int main(int argc, char **argv) {
   
+  uv_loop_t *loop = uv_default_loop();
+  int blocking;
+  
+  /*
+   * TODO(Yorkie): Stuffs
+   */
   QUIP_PRINT("Hello Quip!");
+  
+  uv_run(loop, UV_RUN_DEFAULT);
+  
+  scanf("%d", &blocking);
   return 0;
 }
