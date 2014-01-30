0558767feb49d4969bfb4e317a19555816faa2b1
Yazhong Liu <l900422@vip.qq.com>
2014-01-31 00:00:15 +0800

    lexer: fix some format bug in switch

--- src/lexer.c
+++ src/lexer.c
@@ -55,81 +55,78 @@ int quip_scan(quip_lexer_t * self) {
   // scan
   scan:
   switch (c = next) {
-    case ' ':
-    case '\t': goto scan;
-    case '(': return token(LPAREN);
-    case ')': return token(RPAREN);
-    case '{': return token(LBRACE);
-    case '}': return token(RBRACE);
-    case '[': return token(LBRACK);
-    case ']': return token(RBRACK);
-    case ',': return token(COMMA);
-    case '.': return token(OP_DOT);
-    case '%': return token(OP_MOD);
-    case '^': return token(OP_BIT_XOR);
-    case '~': return token(OP_BIT_NOT);
-    case '?': return token(QMARK);
-    case ':': return token(COLON);
-    case '+':
-      switch (next) {
-        case '=': return token(OP_PLUS_ASSIGN);
-        default: return undo, token(OP_PLUS);
-      }
-    case '-':
-        case '=': return token(OP_MINUS_ASSIGN);
-        default: return undo, token(OP_MINUS);
-      }
-    case '*':
-      switch (next) {
-        case '=': return token(OP_MUL_ASSIGN);
-        default: return undo, token(OP_MUL);
-      }
-    case '/':
-      return '=' == next
-        ? token(OP_DIV_ASSIGN)
-        : (undo, token(OP_DIV));
-    case '!':
-      return '=' == next
-        ? token(OP_NEQ)
-        : (undo, token(OP_NOT));
-    case '=':
-      return '=' == next
-        ? token(OP_EQ)
-        : (undo, token(OP_ASSIGN));
-    case '&':
-      switch (next) {
-        case '&': return token(OP_AND);
-        default:
-          return undo, token(OP_BIT_AND);
-      }
-    case '|':
-      switch (next) {
-        case '|': return token(OP_OR);
-        default:
-          return undo, token(OP_BIT_OR);
-      }
-    case '<':
-      switch (next) {
-        case '=': return token(OP_LTE);
-        case '<': return token(OP_BIT_SHL);
-        default: return undo, token(OP_LT);
-      }
-    case '>':
-      switch (next) {
-        case '=': return token(OP_GTE);
-        case '>': return token(OP_BIT_SHR);
-        default: return undo, token(OP_GT);
-      }
-    case '\n':
-      return scan_newline(self);
-    case '"':
-    case '\'':
-      return scan_string(self, c);
-    case 0:
-      token(EOS);
-      return 0;
-    default:
-      /* TODO */
-      return 0;
+  case ' ':
+  case '\t': goto scan;
+  case '(': return token(LPAREN);
+  case ')': return token(RPAREN);
+  case '{': return token(LBRACE);
+  case '}': return token(RBRACE);
+  case '[': return token(LBRACK);
+  case ']': return token(RBRACK);
+  case ',': return token(COMMA);
+  case '.': return token(OP_DOT);
+  case '%': return token(OP_MOD);
+  case '^': return token(OP_BIT_XOR);
+  case '~': return token(OP_BIT_NOT);
+  case '?': return token(QMARK);
+  case ':': return token(COLON);
+  case '+':
+    switch (next) {
+    case '=': return token(OP_PLUS_ASSIGN);
+    default: return undo, token(OP_PLUS);
+    }
+  case '-':
+    switch (next) {    
+    case '=': return token(OP_MINUS_ASSIGN);
+    default: return undo, token(OP_MINUS);
+    }
+  case '*':
+    switch (next) {
+    case '=': return token(OP_MUL_ASSIGN);
+    default: return undo, token(OP_MUL);
+    }
+  case '/':
+    return '=' == next
+      ? token(OP_DIV_ASSIGN)
+      : (undo, token(OP_DIV));
+  case '!':
+    return '=' == next
+      ? token(OP_NEQ)
+      : (undo, token(OP_NOT));
+  case '=':
+    return '=' == next
+      ? token(OP_EQ)
+      : (undo, token(OP_ASSIGN));
+  case '&':
+    return '&' == next
+      ? token(OP_AND)
+      : (undo, token(OP_BIT_AND));
+  case '|':
+    return '|' == next
+      ? token(OP_OR)
+      : (undo, token(OP_BIT_OR));
+  case '<':
+    switch (next) {
+    case '=': return token(OP_LTE);
+    case '<': return token(OP_BIT_SHL);
+    default: return undo, token(OP_LT);
+    }
+  case '>':
+    switch (next) {
+    case '=': return token(OP_GTE);
+    case '>': return token(OP_BIT_SHR);
+    default: return undo, token(OP_GT);
+    }
+  case '\n':
+    return scan_newline(self);
+  case '"':
+  case '\'':
+    return scan_string(self, c);
+  case 0:
+    token(EOS);
+    return 0;
+  default:
+    /* TODO */
+    return 0;
   }
 }
