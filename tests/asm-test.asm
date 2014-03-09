52cfaeab1acff8e73fc8778166e93b0ba9630204
Yazhong Liu <l900422@vip.qq.com>
2014-03-10 07:15:11 +0800

    add commen to every syscall part

--- tests/asm-test.asm
+++ tests/asm-test.asm
@@ -4,19 +4,22 @@ global main
 
 main:
 
-  push dword _lamb_var_58
-  mov eax, 10
-  sub esp, 4
-  int 128
-
-  push dword _lamb_var_91_len
-  push dword _lamb_var_91
+  ; system call: write
+  push dword _lamb_var_69_len
+  push dword _lamb_var_69
   push dword 1
   mov eax, 4
   sub esp, 4
   int 128
   add esp, 16
 
+  ; system call: unlink
+  push dword _lamb_var_71
+  mov eax, 10
+  sub esp, 4
+  int 128
+
+  ; system call: exit
   push dword 0
   mov eax, 1
   sub esp, 4
@@ -24,7 +27,7 @@ main:
 
 section .data
 
-  _lamb_var_58: db "/opt/asm-test.sample", 0x0a
-  _lamb_var_91: db "Hello worldhdjsahdjs", 0x0a
-  _lamb_var_91_len: equ $-_lamb_var_91
+  _lamb_var_69: db "Hello worldhdjsahdjs", 0x0a
+  _lamb_var_69_len: equ $-_lamb_var_69
+  _lamb_var_71: db "/opt/asm-test.sample", 0x0a
 
