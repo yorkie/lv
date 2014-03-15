542b3a5c839459de498df14c5e21d1d571fd8216
Yazhong Liu <l900422@vip.qq.com>
2014-03-15 21:53:22 +0800

    from javascript to assembly works however it works for `console.log` but it's ok

--- tests/parser-test.asm
+++ tests/parser-test.asm
@@ -4,6 +4,15 @@ global main
 
 main:
 
+  ; system call: write
+  push dword _lamb_var_1_len
+  push dword _lamb_var_1
+  push dword 1
+  mov eax, 4
+  sub esp, 4
+  int 128
+  add esp, 16
+
   ; system call: exit
   push dword 0
   mov eax, 1
@@ -12,5 +21,6 @@ main:
 
 section .data
 
-
+  _lamb_var_1: db "Hello World!", 0x0a
+  _lamb_var_1_len: equ $-_lamb_var_1
 
