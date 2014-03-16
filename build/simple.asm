4b4dae1bd34c3243eac74e7a96d46ff694ede190
Yazhong Liu <l900422@vip.qq.com>
2014-03-16 17:28:55 +0800

    fix \n bug

--- build/simple.asm
+++ build/simple.asm
@@ -5,12 +5,32 @@ global main
 main:
 
   push ebp 
-  push dword helloStr
+
 
 
   ; system call: write
-  push dword x_len
-  push dword x
+  push dword helloStr_len
+  push dword helloStr
+  push dword 1
+  mov eax, 4
+  sub esp, 4
+  int 128
+  add esp, 16
+
+
+  ; system call: write
+  push dword foo_len
+  push dword foo
+  push dword 1
+  mov eax, 4
+  sub esp, 4
+  int 128
+  add esp, 16
+
+
+  ; system call: write
+  push dword bar_len
+  push dword bar
   push dword 1
   mov eax, 4
   sub esp, 4
@@ -27,6 +47,10 @@ main:
 
 section .data
 
-  helloStr: db "1234123", 0x0a
-  x_len: equ $-x
+  helloStr: db 72, 101, 108, 108, 111, 32, 87, 111, 114, 108, 100, 10
+  foo: db 102, 111, 111, 10
+  bar: db 98, 97, 114, 10
+  helloStr_len: equ $-helloStr
+  foo_len: equ $-foo
+  bar_len: equ $-bar
 
--- examples/simple.js
+++ examples/simple.js
@@ -1,3 +1,8 @@
 
-var helloStr = '1234123';
-sys.write(x);
+var helloStr = 'Hello World\n';
+var foo = 'foo\n';
+var bar = 'bar\n';
+
+sys.write(helloStr);
+sys.write(foo);
+sys.write(bar);
