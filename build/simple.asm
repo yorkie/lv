0243eb7b5c47afe1c53eaf9db5f18d4d97cd5ba4
Yazhong Liu <l900422@vip.qq.com>
2014-04-26 18:54:49 +0800

    add `if` statement, but only for literal bcuz that's decided in compiling time.

--- build/simple.asm
+++ build/simple.asm
@@ -17,8 +17,18 @@ main:
 
 
   ; system call: write
-  push dword _lamb_var_95_len
-  push dword _lamb_var_95
+  push dword _lamb_var_42_len
+  push dword _lamb_var_42
+  push dword 1
+  mov eax, 4
+  sub esp, 4
+  int 128
+  add esp, 16
+
+
+  ; system call: write
+  push dword _lamb_var_41_len
+  push dword _lamb_var_41
   push dword 1
   mov eax, 4
   sub esp, 4
@@ -37,6 +47,8 @@ section .data
 
   helloStr: db 72, 101, 108, 108, 111, 32, 87, 111, 114, 108, 100, 10
   helloStr_len: equ $-helloStr
-  _lamb_var_95: db 104, 97, 104, 97, 10
-  _lamb_var_95_len: equ $-_lamb_var_95
+  _lamb_var_42: db 104, 97, 104, 97, 10
+  _lamb_var_42_len: equ $-_lamb_var_42
+  _lamb_var_41: db 98, 101, 101, 112, 10
+  _lamb_var_41_len: equ $-_lamb_var_41
 
--- examples/simple.js
+++ examples/simple.js
@@ -1,5 +1,8 @@
 
 var helloStr = 'Hello World\n';
-
 sys.write(helloStr);
 sys.write('haha\n');
+
+if (123) {
+  sys.write('beep\n');
+}
