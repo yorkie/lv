6cca1bb6d80851f35beeddfa96d34cb24653e75b
Yazhong Liu <yorkiefixer@gmail.com>
2014-08-15 17:38:19 +0800

    base: split literal

--- examples/simple.js
+++ examples/simple.js
@@ -3,7 +3,7 @@ var helloStr = 'Hello World\n';
 var foo = 10;
 
 sys.write(helloStr);
-sys.write(1024);
 sys.write('\n');
-sys.write(true);
-sys.write('\n');
+
+var beep = foo + helloStr;
+// var boop = helloStr + foo;
