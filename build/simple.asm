7aac94c222eb0b6c34cae497e2dff3de094e2887
Yazhong Liu <yorkiefixer@gmail.com>
2014-08-23 16:26:47 +0800

    tweak func

--- build/simple.asm
+++ build/simple.asm
@@ -6,44 +6,26 @@ global main
 
 main:
 
-  push ebp 
-  mov ebp, esp
+	push ebp 
+	mov ebp, esp
 
+	mov eax, 100
+	mov [ebx+16], eax
+	mov eax, 0
+	mov [ebx+18], eax
 
-  ; system call: write
-  push dword helloStr_len
-  push dword helloStr
-  push dword 1
-  mov eax, 4
-  sub esp, 4
-  int 128
-  add esp, 16
+	; system call: exit
+	push dword 0
+	mov eax, 1
+	sub esp, 4
+	int 128
 
-
-  ; system call: write
-  push dword _lamb_var_60_len
-  push dword _lamb_var_60
-  push dword 1
-  mov eax, 4
-  sub esp, 4
-  int 128
-  add esp, 16
-
-  ; system call: exit
-  push dword 0
-  mov eax, 1
-  sub esp, 4
-  int 128
-
-  mov esp, ebp
-  pop ebp
-  ret
+	mov esp, ebp
+	pop ebp
+	ret
 
 section .data
 
-  helloStr: db 72, 101, 108, 108, 111, 32, 87, 111, 114, 108, 100, 10
-  helloStr_len: equ $-helloStr
-  foo: dd 10
-  _lamb_var_60: db 104, 97, 104, 97, 10
-  _lamb_var_60_len: equ $-_lamb_var_60
+	bee: db 72, 101, 108, 108, 111, 32, 87, 111, 114, 108, 100, 10
+	bee_len: equ $-bee
 
--- examples/simple.js
+++ examples/simple.js
@@ -1,4 +1,6 @@
 
 const bee = 'Hello World\n';
+sys.write(bee);
 
-var beep = 1;
+var beep = 100;
+var boop = false;
